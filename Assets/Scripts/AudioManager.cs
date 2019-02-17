73dc3ddbafa0af807f776b9ed17810ff56cdd906
Andrew Merdzinski <AMerdzinski>
2019-02-17 18:59:20 -0500

    Main Menu and Pause Menu (mostly) implemented

---
+++ Assets/Scripts/AudioManager.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour {
+	
+	public AudioClip myclip;
+
+	// Use this for initialization
+	void Start () {
+		this.gameObject.AddComponent<AudioSource>();
+        this.GetComponent<AudioSource>().clip = myclip;
+	}
+	
+	public void Play() {
+		this.GetComponent<AudioSource>().Play();
+		DontDestroyOnLoad(this.gameObject);
+	}
+	
+	public void PlayNew(AudioClip newClip) {
+		this.gameObject.AddComponent<AudioSource>();
+		this.GetComponent<AudioSource>().clip = newClip;
+		this.GetComponent<AudioSource>().Play();
+		DontDestroyOnLoad(this.gameObject);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    //----------------------------------------
+    // handles
+    public SceneTransition UI;
+    //-----------------------------------------
+    // function definitions
+	void Start() 
+	{
+		TogglePauseMenu();
+		
+	}
+
+    public void TogglePauseMenu()
+    {
+		//Toggle timescale on and remove pause menu
+        if (UI.GetComponentInChildren<Canvas>().enabled)
+        {
+            UI.GetComponentInChildren<Canvas>().enabled = false;
+            Time.timeScale = 1.0f;
+		}
+		//Toggle timescale off and show pause menu
+        else
+        {
+            UI.GetComponentInChildren<Canvas>().enabled = true;
+            Time.timeScale = 0f;
+        }
+        
+        Debug.Log("GAMEMANAGER:: TimeScale: " + Time.timeScale);
+    }
+
+}
