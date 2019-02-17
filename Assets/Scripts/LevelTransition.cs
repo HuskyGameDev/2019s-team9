bbf71316adc1e2facd7116b83c276a866a4e0339
Christina Anderson <CAnderson>
2019-02-17 17:49:55 -0500

    Created section one scene, added door/doorway to test transitions, added transition script from previous games, and fixed errors from older packages

---
+++ Assets/Scripts/LevelTransition.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelTransition : MonoBehaviour
+{
+    /// <summary>
+    /// Loads the scene.
+    /// The scene indexes are as follows:
+    ///     MainMenu    = 0
+    ///     LevelSelect = 1
+    ///     QuitGame    = 2
+    ///     Gameplay    = 3
+    /// </summary>
+    /// <param name="scene_index">Scene index.</param>
+    public void LoadScene (int scene_index)
+    {
+        SceneManager.LoadScene (scene_index);
+    }
+
+    /// <summary>
+    /// Quits the game.
+    /// </summary>
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+
+    public GameManager GM;
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		/*
+	    if (Input.GetButtonDown("Cancel"))     
+		{
+			GM.TogglePauseMenu();
+		}
+		*/
+    }
+
+
+
+}
