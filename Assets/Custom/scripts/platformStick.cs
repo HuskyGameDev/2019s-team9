bfc884e4e8cac64460cb9a136697d471b6c83c0a
SawyerKnowles <sdknowle@mtu.edu>
2019-04-21 16:53:11 -0400

    merged all levels together

---
+++ Assets/Custom/scripts/platformStick.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class platformStick : MonoBehaviour {
+
+    public GameObject player;
+    public GameObject buffer;
+    
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject == player)
+        {
+            buffer.transform.parent = transform;
+            player.transform.parent = buffer.transform;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject == player)
+        {
+            buffer.transform.parent = null;
+            player.transform.parent = null;
+        }
+    }
+}
