e4e05ff706f24aecc92ab18a4c5ea7607dda2877
Andrew Merdzinski <AMerdzinski>
2019-04-21 19:44:19 -0400

    Level Select implemented, Pause Menus removed

--- Assets/Scripts/Scripts/GameManager.cs
+++ Assets/Scripts/Scripts/GameManager.cs
@@ -2,15 +2,37 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class GameManager : MonoBehaviour {
-
-	// Use this for initialization
-	void Start () {
+public class GameManager : MonoBehaviour
+{
+    //----------------------------------------
+    // handles
+    public SceneTransition UI;
+    //-----------------------------------------
+    // function definitions
+	void Start() 
+	{
+		TogglePauseMenu();
 		
 	}
 	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    public void TogglePauseMenu()
+    {
+		//Toggle timescale on and remove pause menu
+        if (UI.GetComponentInChildren<Canvas>().enabled)
+        {
+            UI.GetComponentInChildren<Canvas>().enabled = false;
+            Time.timeScale = 1.0f;
+		}
+		//Toggle timescale off and show pause menu
+        else
+        {
+            UI.GetComponentInChildren<Canvas>().enabled = true;
+            Time.timeScale = 0f;
+			GameObject fps = GameObject.Find("FPSController");
+			fps.MouseLook.enabled = false;
+        }
+        
+        Debug.Log("GAMEMANAGER:: TimeScale: " + Time.timeScale);
+    }
+
 }
