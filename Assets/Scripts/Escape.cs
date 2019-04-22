8df81f5fb6333875900ef99740d2a616f02605f7
Kyler Schmidt <KSchmidt>
2019-04-22 17:47:52 -0400

    final build completed

---
+++ Assets/Scripts/Escape.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Escape : MonoBehaviour {
+
+	public float sleepTime = 3f;
+
+	private float time1 = 0f;
+	private float time2 = 0;
+
+	private bool pressedOnce = false;
+
+	// Use this for initialization
+	void Start () {
+			
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if(pressedOnce && Time.time - time1 <= 3f && Input.GetKeyDown(KeyCode.Escape))
+			SceneManager.LoadScene("Main Menu");
+
+		if(time1 == 0f && Input.GetKeyDown(KeyCode.Escape))
+		{
+			pressedOnce = true; 
+			time1 = Time.time;
+			
+		}
+		
+
+
+		
+
+		if(Time.time - time1 >= 3f)
+		{
+			time1 = 0f;
+			pressedOnce = false;
+		}
+			
+	}
+}
---
+++ Assets/Scripts/ResetBlock.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ResetBlock : MonoBehaviour {
+
+	public GameObject block;
+	public GameObject player;
+
+	private void OnTriggerEnter(Collider other) {
+		if(other.gameObject == player)
+			block.transform.position = new Vector3(0.2f,99.1f,520.28f);
+	}
+}
